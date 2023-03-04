e056bd7fcea86498f7645a38fda9c7b2bf4387e0
ilona gricevich <ilonagricevich123@gmail.com>
2023-03-04 22:25:21 +0300

    Homework 8 is done.

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -3,5 +3,12 @@ package Task2;
 public class Main {
     public static void main (String [] args){
 
-       Director = new Director();
+       Director director = new Director();
+       director.getInfo();
+       System.out.println();
+       Worker worker = new Worker();
+       worker.getInfo();
+       System.out.println();
+       Accountant accountant = new Accountant();
+       accountant.getInfo();
     }}
